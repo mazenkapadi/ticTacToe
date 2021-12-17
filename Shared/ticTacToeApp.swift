d767e50af86a821f6a1acfb1c6a610a633820c02
Mazen Kapadi <73120392+mazenkapadi@users.noreply.github.com>
2021-12-17 06:39:28 +0530

    Initial Commit

---
+++ Shared/ticTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ticTacToeApp.swift
+//  Shared
+//
+//  Created by Mazen Kapadi on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct ticTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
